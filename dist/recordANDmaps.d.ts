898fef92b975ebec2e70d49f032e3789bc5fefa7
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-06-22 22:59:34 +0530

    first commit

--- dist/recordANDmaps.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=recordANDmaps.d.ts.map
--- dist/recordANDmaps.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const users = new Map();
-users.set("21323", { age: 21, name: "amaan" });
-users.set("23232", { age: 20, name: "rohan" });
-const user = users.get("21323");
-users.delete('23232');
-console.log(user);
-//# sourceMappingURL=recordANDmaps.js.map
